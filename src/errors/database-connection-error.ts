f734be84f7506d39c41fb3d966a487c61c379480
Mykhailo Yushchenko <mykhailo.yuschenko@ihsmarkit.com>
2020-06-14 10:45:11 +0300

    Updates

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+
+    constructor() {
+        super('Error connecting to database');
+
+        // only because we are extending a built in class
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+        return [{ message: 'Error connecting to database' }];
+    }
+}
